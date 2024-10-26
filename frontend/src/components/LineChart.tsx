0fe013320b3f3984c9eb781f97a9c136d602f4ac
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2024-10-26 21:36:04 +0530

    feat: add line chart component

---
+++ frontend/src/components/LineChart.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title } from 'chart.js';
+import { ApiResponse } from '../api';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title);
+
+interface LineChartProps {
+  data: ApiResponse[];
+}
+
+const LineChart: React.FC<LineChartProps> = ({ data }) => {
+  const chartData = {
+    labels: data.map((_, index) => index + 1),
+    datasets: [
+      {
+        label: 'Change in State',
+        data: data.map((item) => item.changeInState),
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.2,
+      },
+    ],
+  };
+
+  return <Line data={chartData} />;
+};
+
+export default LineChart;
