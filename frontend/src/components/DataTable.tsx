0a11af13dc669281fefb773aa51d25a08e30eb0f
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2024-10-27 08:34:04 +0530

    fix: change table a bit, not fixed fully

--- frontend/src/components/DataTable.tsx
+++ frontend/src/components/DataTable.tsx
@@ -6,48 +6,50 @@ interface DataTableProps {
 }
 
 const DataTable: React.FC<DataTableProps> = ({ data }) => (
-  <table className="data-table">
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>Input</th>
-        <th>Polarity</th>
-        <th>Features</th>
-        <th colSpan={2}>Categories</th>
-        <th>Change in State</th>
-      </tr>
-      <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th>Category</th>
-        <th>Intensity</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      {data.map((item, index) => (
-        <React.Fragment key={index}>
-          <tr>
-            <td rowSpan={item.response.categories.length}>{index + 1}</td>
-            <td rowSpan={item.response.categories.length}>{item.inputText}</td>
-            <td rowSpan={item.response.categories.length}>{item.response.polarity}</td>
-            <td rowSpan={item.response.categories.length}>{item.response.features.join(', ')}</td>
-            <td>{item.response.categories[0]}</td>
-            <td>{item.response.intensity[0]}</td>
-            <td rowSpan={item.response.categories.length}>{item.response.changeInState}</td>
-          </tr>
-          {item.response.categories.slice(1).map((category, catIndex) => (
-            <tr key={`${index}-${catIndex}`}>
-              <td>{category}</td>
-              <td>{item.response.intensity[catIndex + 1]}</td>
+  <div style={{ overflowX: 'auto' }}>
+    <table className="data-table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Input</th>
+          <th>Polarity</th>
+          <th>Features</th>
+          <th colSpan={2}>Categories</th>
+          <th>Change in State</th>
+        </tr>
+        <tr>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th>Category</th>
+          <th>Intensity</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <React.Fragment key={index}>
+            <tr>
+              <td rowSpan={item.response.categories.length}>{index + 1}</td>
+              <td rowSpan={item.response.categories.length}>{item.inputText}</td>
+              <td rowSpan={item.response.categories.length}>{item.response.polarity}</td>
+              <td rowSpan={item.response.categories.length}>{item.response.features.join(', ')}</td>
+              <td>{item.response.categories[0]}</td>
+              <td>{item.response.intensity[0]}</td>
+              <td rowSpan={item.response.categories.length}>{item.response.changeInState}</td>
             </tr>
-          ))}
-        </React.Fragment>
-      ))}
-    </tbody>
-  </table>
+            {item.response.categories.slice(1).map((category, catIndex) => (
+              <tr key={`${index}-${catIndex}`}>
+                <td>{category}</td>
+                <td>{item.response.intensity[catIndex + 1]}</td>
+              </tr>
+            ))}
+          </React.Fragment>
+        ))}
+      </tbody>
+    </table>
+  </div>
 );
 
 export default DataTable;
