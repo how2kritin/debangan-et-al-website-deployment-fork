b5fe52979839dc58511be602aab1277dfb24d58a
Varun0157 <varun.edachali@gmail.com>
2024-10-27 06:23:59 +0530

    threshold category intensity

--- backend/data_processor.py
+++ backend/data_processor.py
@@ -16,14 +16,14 @@ CONCERN_REANGES = [4, 4]
 
 
 def add_scores(a: List[int], b: List[int]):
-    return [max(a[i], b[i]) for i in range(len(a))] # hacky bitwise or
+    return [max(a[i], b[i]) for i in range(len(a))]  # hacky bitwise or
 
 
 def get_cur_day_scores():
     def accumulate_scores(scores_list: List[utils.DateScores]) -> utils.DateScores:
         aggregate: utils.DateScores = scores_list[-1]
 
-        for date_score in scores_list[: -1]:
+        for date_score in scores_list[:-1]:
             for key in aggregate[1]:
                 aggregate[1][key] = add_scores(aggregate[1][key], date_score[1][key])
 
@@ -46,21 +46,27 @@ def update_cur_day_scores(scores_to_add: utils.DateScores) -> utils.DateScores:
     return cur_day_scores
 
 
-def calculate_phq_scores(week_scores: List[utils.DateScores], concern_ranges: List[int]):
-    num_cols = {concern: len(week_scores[0][1][concern]) for concern in week_scores[0][1]}
+def calculate_phq_scores(
+    week_scores: List[utils.DateScores], concern_ranges: List[int]
+):
+    num_cols = {
+        concern: len(week_scores[0][1][concern]) for concern in week_scores[0][1]
+    }
     scores = {key: [0 for _ in range(num_cols[key])] for key in num_cols}
 
     for day in range(len(week_scores)):
         for concern in num_cols:
             for col in range(num_cols[concern]):
                 scores[concern][col] += week_scores[day][1][concern][col]
-    
+
     # changing score from list to int
     i = 0
     concern_score: Dict[str, int] = {key: 0 for key in num_cols}
     for concern in scores:
         for col in range(num_cols[concern]):
-            concern_score[concern] += math.floor(scores[concern][col] * concern_ranges[i] / 7.1)
+            concern_score[concern] += math.floor(
+                scores[concern][col] * concern_ranges[i] / 7.1
+            )
         i += 1
 
     return concern_score
@@ -71,14 +77,16 @@ def get_cur_day_phq_scores(input_text: str):
 
     cur_date = utils.get_cur_date()
     # TODO: plug in model outputs here when done
-    todays_scores = utils.get_random_date_scores(cur_date, DATE_SCORES_SIZES, DATE_SCORES_LABELS)
+    todays_scores = utils.get_random_date_scores(
+        cur_date, DATE_SCORES_SIZES, DATE_SCORES_LABELS
+    )
 
     # update cur date in file
     if utils.is_cur_date_in_file(JSONFILE):
         todays_scores = update_cur_day_scores(todays_scores)
     else:
         utils.append_to_file(JSONFILE, [todays_scores])
-    
+
     scores = calculate_phq_scores(utils.read_last_entries(JSONFILE, 7), CONCERN_REANGES)
     return list(scores.keys()), list(scores.values())
 
@@ -86,9 +94,11 @@ def get_cur_day_phq_scores(input_text: str):
 def get_cur_day_concern_labels(scores: list[int]):
     # TODO: write actual shite
     return [str(key) for key in scores]
-    
 
-def _predict_categories(input_text: str, num_cats: int) -> Dict[str, int]:
+
+def _predict_categories(
+    input_text: str, num_cats: int, threshold: float = 0.1
+) -> Dict[str, int]:
     categories = utils.get_categories()
 
     if len(input_text) < 1:
@@ -100,7 +110,9 @@ def _predict_categories(input_text: str, num_cats: int) -> Dict[str, int]:
     prompt = f"{input_text} I should be diagnosed with [MASK]."
     results = unmask_category(prompt)
     filtered_results = [
-        result for result in results if result["token_str"] in categories
+        result
+        for result in results
+        if result["token_str"] in categories and result["score"] >= threshold
     ]
 
     if len(filtered_results) < 1:
@@ -136,10 +148,10 @@ def process_data(input_text: str):
     return {
         "polarity": polarity,
         "features": features,
-        "categories": categories, # { category : intensity }
+        "categories": categories,  # { category : intensity }
         "intensity": intensities,
         "currentDate": currentDate,
         "categoryNames": concernNames,
         "categoryScores": cur_day_phq_scores,
-        "categoryLabels": cur_day_concern_labels
+        "categoryLabels": cur_day_concern_labels,
     }
