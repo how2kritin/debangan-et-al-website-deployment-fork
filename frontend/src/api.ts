e3b4301b54e91476b35a102010f496b255e640ab
how2kritin <kritinmaddired@gmail.com>
2024-10-27 15:24:35 +0530

    ok

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -15,7 +15,7 @@ export interface ApiResponse {
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export async function fetchApiData(input: string, isLoading: Function): Promise<ApiResponse> {
   isLoading(true);
-  const response = await axios.post<ApiResponse>('http://debangan-et-al-megathon-2k24-production.up.railway.app:8080/api/process', { input });
+  const response = await axios.post<ApiResponse>('https://debangan-et-al-megathon-2k24-production.up.railway.app:8080/api/process', { input });
   isLoading(false);
   return response.data;
 }
