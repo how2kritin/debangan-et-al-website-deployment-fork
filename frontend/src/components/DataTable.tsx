37cb472b1f2a273db3cce5f981b2ac01b6a94eb4
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2024-10-27 08:04:45 +0530

    feat: show each category in table

--- frontend/src/components/DataTable.tsx
+++ frontend/src/components/DataTable.tsx
@@ -9,26 +9,42 @@ const DataTable: React.FC<DataTableProps> = ({ data }) => (
   <table className="data-table">
     <thead>
       <tr>
-        <th>#</th> {/* Serial number header */}
+        <th>#</th>
         <th>Input</th>
         <th>Polarity</th>
         <th>Features</th>
-        <th>Categories</th>
-        <th>Intensity</th>
+        <th colSpan={2}>Categories</th>
         <th>Change in State</th>
+      </tr>
+      <tr>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th>Category</th>
+        <th>Intensity</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
       {data.map((item, index) => (
-        <tr key={index}>
-          <td>{index + 1}</td> {/* Serial number cell */}
-          <td>{item.inputText}</td>
-          <td>{item.response.polarity}</td>
-          <td>{item.response.features}</td>
-          <td>{item.response.categories}</td>
-          <td>{item.response.intensity}</td>
-          <td>{item.response.changeInState}</td>
-        </tr>
+        <React.Fragment key={index}>
+          <tr>
+            <td rowSpan={item.response.categories.length}>{index + 1}</td>
+            <td rowSpan={item.response.categories.length}>{item.inputText}</td>
+            <td rowSpan={item.response.categories.length}>{item.response.polarity}</td>
+            <td rowSpan={item.response.categories.length}>{item.response.features.join(', ')}</td>
+            <td>{item.response.categories[0]}</td>
+            <td>{item.response.intensity[0]}</td>
+            <td rowSpan={item.response.categories.length}>{item.response.changeInState}</td>
+          </tr>
+          {item.response.categories.slice(1).map((category, catIndex) => (
+            <tr key={`${index}-${catIndex}`}>
+              <td>{category}</td>
+              <td>{item.response.intensity[catIndex + 1]}</td>
+            </tr>
+          ))}
+        </React.Fragment>
       ))}
     </tbody>
   </table>
